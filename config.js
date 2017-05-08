5f2566de7848ae15b2ba878d2ff6007dd8477d2f
bhankee <brad.hankee@gmail.com>
2017-05-08 17:27:27 -0500

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  consumer_key: 'rMZ2fUE7mapPm4LeIKlPb2hj2',
-  consumer_secret: 'VOoBsRW9emPA8SQaMfgFuFZR0QsK7QTDeCICHnW093z8WWHmr0',
-  access_token: '720290730187563008-uPRA7z2gp36LafxVWwAi14vIcdPe7Dv',
-  access_token_secret: 'MB9X0M0UwjT2I6IkqjyZsNyHXTo3gmSTxYIHs3Up354h7'
-}
